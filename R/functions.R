978286977c3b085c0bbb74820115088a363ac2a4
Malcolm Morgan <mem48@users.noreply.github.com>
2023-03-02 15:52:46 +0000

    Getting started with leeds

--- R/functions.R
+++ R/functions.R
@@ -1,7 +1,39 @@
 ### Return an OSM Overpass query as an osmdata object in sf format
 download_osm = function(place){
+  
+  if(!curl::has_internet()){
+    assign("has_internet_via_proxy", TRUE, environment(curl::has_internet))
+  }
+  
   q = osmdata::opq(osmdata::getbb(place))
   q = osmdata::add_osm_feature(q, key = "highway")
   osm_raw = osmdata::osmdata_sf(q = q)
   return(osm_raw)
+}
+
+
+prepare_traffic_data = function(path, year){
+  traffic <- read.csv(file=path, header=TRUE)
+  
+  # Change the lon and lat to the 4326 crs
+  traffic <- sf::st_as_sf(traffic,coords = c("longitude","latitude"),crs = 4326) 
+  
+  # Filter the count points in year
+  traffic <- traffic[traffic$year == year, ] 
+  traffic <- traffic[ , c("road_name","road_type","all_motor_vehicles")] 
+  
+  names(traffic)<-c("road_name","road_type","traffic_flow","geometry")
+  traffic <- sf::st_transform(traffic,27700)
+  
+  return(traffic)
+}
+
+make_boundary = function(x){
+  # Make Polygon Around Traffic Data
+  bound_buf <- sf::st_convex_hull(sf::st_union(x)) 
+  
+  # Buffer Polygon by 1km
+  bound_buf <- sf::st_buffer(bound_buf, 1000) 
+  
+  return(bound_buf)
 }
--- R/leeds_2018.R
+++ R/leeds_2018.R
@@ -1,2 +1,18 @@
+source("R/functions.R")
+library(tmap)
+tmap_mode("view")
+
+# Get OSM Data
 osm_leeds = download_osm("Leeds, UK")
 saveRDS(osm_leeds, "data/osm_leeds_raw.Rds")
+
+# Get Traffic Counts
+traffic_leeds_2018 = prepare_traffic_data("data/dft_aadf_local_authority_id_63.csv", year = 2018)
+saveRDS(traffic_leeds_2018, "data/traffic_leeds_2018.Rds")
+
+# Make a boundary around the traffic data
+boundary = make_boundary(traffic_leeds_2018)
+saveRDS(boundary, "data/leeds_boundary.Rds")
+
+qtm(boundary, fill = NULL) +
+qtm(traffic_leeds_2018, dots.col = "traffic_flow")
